7a40039697de73e6e7336eeb7013ec091fac2459
Kateryna-k9 <Kateryna.Kryndach@cs.khpi.edu.ua>
2025-09-28 17:51:03 +0300

    lab 1_3

---
+++ Term1/lab 1/Lab 1_3/Lab 1_3.cpp
@@ -0,0 +1,17 @@
+﻿// Lab 1_3.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <cstdio>
+
+int main() {
+    int x = 2;  
+
+    x = x * x;  
+    x = x * x;  
+    x = x * x;  
+
+    printf("x to the 8th power = %d\n", x);
+
+    return 0;
+}
