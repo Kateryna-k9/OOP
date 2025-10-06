6564c9ad7c587d1bdd45dab40939f6f5940c721a
Kateryna-k9 <Kateryna.Kryndach@cs.khpi.edu.ua>
2025-10-06 21:04:51 +0300

    Lab 2

--- Term1/lab 1/Lab 1_5/Lab 1_5.cpp
+++ Term1/lab 1/Lab 1_5/Lab 1_5.cpp
@@ -3,33 +3,23 @@
 
 #include <iostream>
 #include <cstdio>
-#include <climits> 
 
 int main() {
-    int seconds_in_year = 365 * 24 * 60 * 60; 
+    const int seconds_in_day = 24 * 60 * 60;     // 86400
+    const int days_in_year = 365;
 
-    int years[] = { 100, 1000, 10000, 100000, 1000000, 10000000 };
-
-    for (int i = 0; i < 6; ++i) {
-        int y = years[i];
-
-        long long total = (long long)seconds_in_year * y;
-
-        if (total <= INT_MAX) {
-            int result = seconds_in_year * y;
-            printf("%d years: %d seconds [int]\n", y, result);
-        }
-
-        if (total > INT_MAX && total <= LONG_MAX) {
-            long result = (long)total;
-            printf("%d years: %ld seconds [long]\n", y, result);
-        }
-
-        if (total > LONG_MAX) {
-            long long result = total;
-            printf("%d years: %lld seconds [long long]\n", y, result);
-        }
-    }
+    printf("In 1 year: %llu seconds\n", 1ULL * days_in_year * seconds_in_day);
+    printf("In 10 years: %llu seconds\n", 10ULL * days_in_year * seconds_in_day);
+    printf("In 100 years: %llu seconds\n", 100ULL * days_in_year * seconds_in_day);
+    printf("In 1000 years: %llu seconds\n", 1000ULL * days_in_year * seconds_in_day);
+    printf("In 10000 years: %llu seconds\n", 10000ULL * days_in_year * seconds_in_day);
+    printf("In 100000 years: %llu seconds\n", 100000ULL * days_in_year * seconds_in_day);
+    printf("In 1000000 years: %llu seconds\n", 1000000ULL * days_in_year * seconds_in_day);
+    printf("In 10000000 years: %llu seconds\n", 10000000ULL * days_in_year * seconds_in_day);
+    printf("In 100000000 years: %llu seconds\n", 100000000ULL * days_in_year * seconds_in_day);
+    printf("In 1000000000 years: %llu seconds\n", 1000000000ULL * days_in_year * seconds_in_day);
 
     return 0;
 }
+
+
