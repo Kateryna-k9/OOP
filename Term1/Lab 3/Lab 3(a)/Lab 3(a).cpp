180c95af4615f9a661166897553c61d0ba2b3ef8
Kateryna-k9 <Kateryna.Kryndach@cs.khpi.edu.ua>
2025-11-30 19:18:50 +0200

    commit

--- Term1/Lab 3/Lab 3(a)/Lab 3(a).cpp
+++ Term1/Lab 3/Lab 3(a)/Lab 3(a).cpp
@@ -1,69 +1,77 @@
-﻿// Lab 3(a).cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
-#define  _CRT_SECURE_NO_WARNINGS
-#include <iostream>
-#include "windows.h"
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
 #include <math.h>
-using namespace std;
-
+#include <windows.h>
 
 int main() {
+
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
     while (true) {
 
-        SetConsoleCP(1251);
-        SetConsoleOutputCP(1251);
+        printf("Знаходження коренів квадратного рівняння ax² + bx + c = 0\n\n");
 
+        double a, b, c;
 
-        printf("Лабораторна робота №3А\n");
-        printf("Знаходження коренів квадратного рівняння виду ax² + bx + c = 0\n");
-
-        int a, b, c;
+        
         printf("Введіть a: ");
-        scanf("%d", &a);
-        printf("Введіть b: ");
-        scanf("%d", &b);
-        printf("Введіть c: ");
-        scanf("%d", &c);
-
-        printf("Введіть a: ");
-        if (scanf("%d", &a) != 1) {
-            printf("Введено не цифру!\n");
-            break;
+        if (scanf("%lf", &a) != 1) {
+            printf("Помилка! Ви ввели не число.\n");
+            while (getchar() != '\n');
+            continue;
         }
 
+        printf("Введіть b: ");
+        if (scanf("%lf", &b) != 1) {
+            printf("Помилка! Ви ввели не число.\n");
+            while (getchar() != '\n');
+            continue;
+        }
+
+        printf("Введіть c: ");
+        if (scanf("%lf", &c) != 1) {
+            printf("Помилка! Ви ввели не число.\n");
+            while (getchar() != '\n');
+            continue;
+        }
+
+        printf("\n");
+
+        
         if (a == 0) {
-            printf("Це не квадратне рівняння!\n");
+            printf("Це не квадратне рівняння.\n");
 
             if (b != 0) {
-                double x = -(double)c / b;
-                printf("Корінь рівняння: x = %.2f\n", x);
+                double x = -c / b;
+                printf("Корінь рівняння: x = %.2lf\n\n", x);
             }
             else {
                 if (c == 0)
-                    printf("Рівняння має безліч розв’язків.\n");
+                    printf("Рівняння має безліч розв'язків.\n\n");
                 else
-                    printf("Рівняння не має розв’язків.\n");
+                    printf("Рівняння не має розв'язків.\n\n");
             }
         }
         else {
-            int D = b * b - 4 * a * c;
-            printf("Дискримінант D = %d\n", D);
+            double D = b * b - 4 * a * c;
+            printf("Дискримінант D = %.2lf\n", D);
 
             if (D > 0) {
-                double x1 = (-b + sqrt(D)) / (2.0 * a);
-                double x2 = (-b - sqrt(D)) / (2.0 * a);
+                double x1 = (-b + sqrt(D)) / (2 * a);
+                double x2 = (-b - sqrt(D)) / (2 * a);
                 printf("Рівняння має два різних дійсних корені:\n");
-                printf("x₁ = %.2f, x₂ = %.2f\n", x1, x2);
+                printf("x₁ = %.2lf, x₂ = %.2lf\n\n", x1, x2);
             }
             else if (D == 0) {
-                double x = -b / (2.0 * a);
-                printf("Рівняння має один дійсний корінь: x = %.2f\n", x);
+                double x = -b / (2 * a);
+                printf("Рівняння має один дійсний корінь: x = %.2lf\n\n", x);
             }
             else {
-                printf("Рівняння не має дійсних коренів (D < 0).\n");
+                printf("Рівняння не має дійсних коренів (D < 0).\n\n");
             }
         }
+
+
     }
-    return 0;
 }
